ca654e4cf8a9488447e745807510f4974d90f611
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-07-31 23:48:48 +0900

    overRoad interface

---
+++ chapter5/overRoad.ts
@@ -0,0 +1,19 @@
+interface Bounds {
+    width: number
+    height: number
+    move(amount: string): string
+}
+interface Bounds {
+    left: number
+    top: number
+    move(amount: number): string
+}
+const bounds: Bounds = {
+    width: 0,
+    height: 0,
+    left: 0,
+    top: 0,
+    move: (amount: string | number) => {
+        return `${amount}`
+    }
+}
