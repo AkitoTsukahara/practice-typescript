f40d190d4ff0203a176ef376bb924d61efbb6ea2
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-08-10 23:47:02 +0900

    data型

---
+++ vue-typescript/src/components/AppTodoList.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Vue from 'Vue'
+
+export type Todo = {
+    id: String
+    task: String
+    done: boolean
+}
+
+export type Data = {
+    firstName: String
+    lastName: String
+    todos: Todo[]
+}
+
+export default Vue.extend({
+    data: (): Data => ({
+        firstName: 'Taro',
+        lastName: 'Yamada',
+        todos: []
+    })
+})
+</script>
