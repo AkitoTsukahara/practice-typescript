494a84c13b37ae4d5a5a708bc428ed4fd1020c2a
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-08-22 15:11:52 +0900

    fix props type

--- vue-typescript/src/components/AppTodoItem.vue
+++ vue-typescript/src/components/AppTodoItem.vue
@@ -1,25 +1,28 @@
 <template>
-    <div class="todo">
-        {{maYask}}
-    </div>
+  <div class="todo">
+    {{ maYask }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,PropType } from 'vue'
+import {defineComponent, PropType} from 'vue'
 
 export type Todo = {
-    id: string
-    task: string
-    done: boolean
+  id: string
+  task: string
+  done: boolean
 }
 export default defineComponent({
-    props: {
-        todo: Object as PropType<Todo>
-    },
-    computed: {
-        myTask(): string {
-            return this?.todo?.task?
-        }
+  props: {
+    todo: {
+      type: Object as PropType<Todo>,
+      required: true
     }
+  },
+  computed: {
+    myTask(): string {
+      return this.todo.task
+    }
+  }
 })
 </script>
