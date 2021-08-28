9ea27231769e9cc2aff9924e6218e81e83dc6577
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-08-28 23:55:57 +0900

    components

--- vue-typescript/src/components/AppTodoList.vue
+++ vue-typescript/src/components/AppTodoList.vue
@@ -8,7 +8,6 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
 import AppTodoItem, { Todo } from './AppTodoItem.vue'
 
 export type Data = {
---
+++ vue-typescript/src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data: () => ({
+    defaultDateLabel: "1980-01-01",
+    dateLabel: null as string | null
+  }),
+  computed: {
+    dateFromDateLabel(): Date {
+      let label = this.defaultDateLabel
+      if (this.dateLabel !== null) {
+        label = this.dateLabel
+      }
+      return new Date(label)
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
